fbcce7dab59479b1aff32a110128f424e53d354b
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-11-10 22:53:49 +0530

    phone and framer motion updated

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -1,28 +1,29 @@
 import React from "react";
 import Button from "../../Common/Button/Button";
-import styles from "./styles.module.css"
+import "./styles.css";
 import {motion} from "framer-motion";
+import PhoneComponent from "./PhoneComponent";
 
 function LandingIntro() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.info}>
+    <div className="wrapper">
+      <div className="info">
         <motion.h1 
-        className={styles.bigHeading}
+        className="bigHeading"
         initial={{y:50, opacity:0}}
         whileInView={{y:0, opacity:1}}
         transition={{duration:.5}}
         >
           Track Crypto
           </motion.h1>
-        <motion.h1 className={styles.bigHeading2}
+        <motion.h1 className="bigHeading2"
         initial={{y:50, opacity:0}}
         whileInView={{y:0, opacity:1}}
         transition={{duration:0.5}}
         
         >
           Real Time.</motion.h1>
-        <motion.p className={styles.para}
+        <motion.p className="para"
         initial={{y:50, opacity:0}}
         whileInView={{y:0, opacity:1}}
         transition={{duration:0.5}}
@@ -30,7 +31,7 @@ function LandingIntro() {
           Track crypto through a public api in real time. Visit the dashboard to
           do so!
         </motion.p>
-        <motion.div className={styles.flexBtn}
+        <motion.div className="flexBtn"
         initial={{y:50, opacity:0}}
         whileInView={{y:0, opacity:1}}
         transition={{duration:0.5}}
@@ -39,7 +40,7 @@ function LandingIntro() {
           <Button text="Share" outlined={true} />
         </motion.div>
       </div>
-      <div className={styles.phone}></div>
+      <div><PhoneComponent/></div>
     </div>
   );
 }
