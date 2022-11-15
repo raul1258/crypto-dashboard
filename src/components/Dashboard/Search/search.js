ab8c14e2fb07b932a3821c1f51240dfb0f35661c
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-11-15 22:45:12 +0530

    search bar

---
+++ src/components/Dashboard/Search/search.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./style.css";
+
+function Search({search,setSearch}) {
+  return (
+    <div className="search-box">
+      <SearchIcon sx={{ color: "var(--grey) !important" }} />
+      <input
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      type="text"
+      placeholder="Search"
+
+      />
+    </div>
+  );
+}
+
+export default Search;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Search from "../components/Dashboard/Search/search";
 import Header from "../components/Common/Header";
 import Tabs from "../components/Dashboard/Tabs/tabs";
 import { DASHBOARD_API_URL } from "../constants";
 
 function DashboardPage() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
 
   useEffect(() => {
     axios
@@ -19,11 +22,24 @@ function DashboardPage() {
       });
   }, []);
 
+  var filteredCoins = data.filter((item) => {
+    if (
+      item.symbol.toLowerCase().includes(search.toLowerCase()) ||
+      item.name.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return item;
+    }
+  });
+
+
   return (
     <div>
       <Header />
-      <Tabs data={data} />
-      
+      <Search search={search} setSearch={setSearch} />
+
+
+      <Tabs data={filteredCoins} />
+
     </div>
   );
 }
