20c456b74458adb386b4d2d5f111b22db63bfd4c
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-11-18 23:24:32 +0530

    coin page and compare page broke

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import DashboardPage from "./pages/dashboard";
 import CoinPage from "./pages/coin";
+import ComparePage from "./pages/compare";
+
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/coin.js
+++ src/pages/coin.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import LineChart from "../components/Coin/Chart";
 import Info from "../components/Coin/Info/info";
+import SelectDays from "../components/Coin/SelectDays/selectDays";
+import TogglePrice from "../components/Coin/ToggleComponent/toggle";
 import Header from "../components/Common/Header";
 import Loading from "../components/Common/Loading/loading";
 import List from "../components/Dashboard/ListComponent/List";
-import LineChart from "../components/Coin/Chart";
-import {getCoinData} from "../functions/getCoinData";
-import {getCoinPrices} from "../functions/getCoinPrices"
-import {getDate} from "../functions/getDate";
-
+import { convertNumbers } from "../functions/convertNumber";
+import { getCoinData } from "../functions/getCoinData";
+import { getCoinPrices } from "../functions/getCoinPrices";
+import { getDate } from "../functions/getDate";
 
 function CoinPage() {
   const { id } = useParams();
   const [coin, setCoin] = useState({});
-  const [loading, setLoading] = useState(true);
   const [days, setDays] = useState(120);
+  const [loading, setLoading] = useState(true);
+  const [priceType, setPriceType] = useState("prices");
 
   const [chartData, setChartData] = useState({
     labels: [],
@@ -33,52 +36,144 @@ function CoinPage() {
       mode: "index",
       intersect: false,
     },
+    scales: {
+      y: {
+        ticks:
+          priceType === "market_caps"
+            ? {
+                callback: function (value) {
+                  return "$" + convertNumbers(value);
+                },
+              }
+            : priceType === "total_volumes"
+            ? {
+                callback: function (value) {
+                  return convertNumbers(value);
+                },
+              }
+            : {
+                callback: function (value, index, ticks) {
+                  return "$" + value.toLocaleString();
+                },
+              },
+      },
+    },
   };
-
 
   useEffect(() => {
     if (id) {
-      axios
-        .get(`https://api.coingecko.com/api/v3/coins/${id}`)
-        .then((response) => {
-          console.log("Response Data >>>", response.data);
-          setCoin({
-            id: response.data.id,
-            name: response.data.name,
-            symbol: response.data.symbol,
-            image: response.data.image.large,
-            desc : response.data.description.en,
-            price_change_percentage_24h:
-            response.data.market_data.price_change_percentage_24h,
-            total_volume: response.data.market_data.total_volume.usd,
-            current_price: response.data.market_data.current_price.usd,
-            market_cap: response.data.market_data.market_cap.usd,
-          });
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log("Error>>>", error);
-        });
+      getData();
     }
   }, [id]);
 
+  const getData = async () => {
+    const data = await getCoinData(id);
+    const prices = await getCoinPrices(id, days, priceType);
+
+    if (data) {
+      console.log("data", data);
+      setCoin({
+        id: data.id,
+        name: data.name,
+        symbol: data.symbol,
+        image: data.image.large,
+        desc: data.description.en,
+        price_change_percentage_24h:
+          data.market_data.price_change_percentage_24h,
+        total_volume: data.market_data.total_volume.usd,
+        current_price: data.market_data.current_price.usd,
+        market_cap: data.market_data.market_cap.usd,
+      });
+      setLoading(false);
+    }
+    if (prices) {
+      console.log("Prices>>>", prices);
+      setChartData({
+        labels: prices?.map((data) => getDate(data[0])),
+        datasets: [
+          {
+            label: "Price",
+            data: prices?.map((data) => data[1]),
+            borderWidth: 1,
+            fill: false,
+            tension: 0.25,
+            backgroundColor: "transparent",
+            borderColor: "#3a80e9",
+            pointRadius: 0,
+          },
+        ],
+      });
+    }
+  };
+
+  const handleDaysChange = async (event) => {
+    const prices = await getCoinPrices(id, event.target.value, priceType);
+    if (prices) {
+      setChartData({
+        labels: prices?.map((data) => getDate(data[0])),
+        datasets: [
+          {
+            label: "Price",
+            data: prices?.map((data) => data[1]),
+            borderWidth: 1,
+            fill: false,
+            tension: 0.25,
+            backgroundColor: "transparent",
+            borderColor: "#3a80e9",
+            pointRadius: 0,
+          },
+        ],
+      });
+    }
+    setDays(event.target.value);
+  };
+
+  const handlePriceChange = async (event) => {
+    setPriceType(event.target.value);
+    const prices = await getCoinPrices(id, days, event.target.value);
+    if (prices) {
+      setChartData({
+        labels: prices?.map((data) => getDate(data[0])),
+        datasets: [
+          {
+            label: "Price",
+            data: prices?.map((data) => data[1]),
+            borderWidth: 1,
+            fill: false,
+            tension: 0.25,
+            backgroundColor: "transparent",
+            borderColor: "#3a80e9",
+            pointRadius: 0,
+          },
+        ],
+      });
+    }
+  };
+
   return (
     <div>
-    <Header />
-    {loading ? (
-      <Loading />
-    ) : (
-      <>
-        <div className="grey-container">
-          <List coin={coin} />
-        </div>
-        {/* <div className="grey-container">
-          <LineChart chartData={chartData} options={options} />
-        </div> */}
-        <Info name={coin.name} desc={coin.desc} />
-      </>
-    )}
-  </div>
+      <Header />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="grey-container">
+            <List coin={coin} delay={0.5} />
+          </div>
+          <div className="grey-container">
+            <SelectDays days={days} handleChange={handleDaysChange} />
+            <TogglePrice
+              priceType={priceType}
+              handleChange={handlePriceChange}
+            />
+            <LineChart chartData={chartData} options={options} />
+          </div>
+          <div className="grey-container">
+            <Info name={coin.name} desc={coin.desc} />
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
