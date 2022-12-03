4aa31cbc1fe021cdc41fb3c27e629b39ab2c1b03
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-12-03 21:19:53 +0530

    completd

--- src/components/Dashboard/Search/search.js
+++ src/components/Dashboard/Search/search.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import "./style.css";
+import { BorderColor } from "@mui/icons-material";
 
 function Search({search,setSearch}) {
   return (
     <div className="search-box">
-      <SearchIcon sx={{ color: "var(--grey) !important" }} />
+      <SearchIcon sx={{ color: "var(--grey) !important"}} />
       <input
       value={search}
       onChange={(e) => setSearch(e.target.value)}
